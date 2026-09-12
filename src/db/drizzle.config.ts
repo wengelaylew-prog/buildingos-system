102f1279dfc4f8f02553c831934e4ce1d0bb9269
wengelaylew-prog <wengelaylew@gmail.com>
2026-09-11 18:06:16 -0700

    fix: drizzle config support URL

--- src/db/drizzle.config.ts
+++ src/db/drizzle.config.ts
@@ -1,39 +1,24 @@
 import { defineConfig } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
 
-// Load environment variables from .env file.
 dotenv.config();
-
-const sqlHost = process.env.SQL_HOST;
-const sqlDbName = process.env.SQL_DB_NAME;
-const user = process.env.SQL_ADMIN_USER;
-const password = process.env.SQL_ADMIN_PASSWORD;
-
-if (!sqlHost) {
-  throw new Error('SQL_HOST must be set in environment variables.');
-}
-if (!sqlDbName) {
-  throw new Error('SQL_DB_NAME must be set in environment variables.');
-}
-if (!user) {
-  throw new Error('SQL_ADMIN_USER must be set in environment variables.');
-}
-if (!password) {
-  throw new Error('SQL_ADMIN_PASSWORD must be set in environment variables.');
-}
-console.log(`Using user: ${user} to connect to database.`);
 
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   schemaFilter: ['public'],
-  dbCredentials: {
-    host: sqlHost,
-    user: user,
-    password: password,
-    database: sqlDbName,
-    ssl: true,
-  },
+  dbCredentials: process.env.DATABASE_URL
+    ? {
+        url: process.env.DATABASE_URL,
+      }
+    : {
+        host: process.env.SQL_HOST!,
+        user: process.env.SQL_ADMIN_USER!,
+        password: process.env.SQL_ADMIN_PASSWORD!,
+        database: process.env.SQL_DB_NAME!,
+        ssl: true,
+      },
   verbose: true,
 });
+
